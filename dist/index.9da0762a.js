10f2c9fe7d1ebbd3ffd7202505ba612880fe7959
Özgur Celik <ozce2300@student.miun.se>
2024-03-18 23:50:04 +0100

    Endast 10 data visas åtgången, finns knapp som kan visa 10 fler när man trycker.

--- dist/index.9da0762a.js
+++ dist/index.9da0762a.js
@@ -585,17 +585,22 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"dcsaV":[function(require,module,exports) {
 let mapCount = 0;
+let displayedEvents = 0;
+let eventsPerPage = 10;
+let visaMerEl = document.getElementById("visa-mer-data");
+visaMerEl.addEventListener("click", displayMoreEvents);
 async function getData() {
     try {
         let response = await fetch("https://polisen.se/api/events");
         let data = await response.json();
-        getDisplay(data);
+        displayData(data);
     } catch (error) {
         console.log("Error", error);
     }
 }
-function getDisplay(data) {
-    data.forEach((item)=>{
+function displayData(data) {
+    const mainEL = document.getElementById("main-show");
+    data.slice(displayedEvents, displayedEvents + eventsPerPage).forEach((item)=>{
         let name = item.type;
         let summary = item.summary;
         let text = item.name;
@@ -604,35 +609,36 @@ function getDisplay(data) {
         let lonlat = gps.split(",");
         let lat = lonlat[0];
         let lon = lonlat[1];
-        let mainEL = document.getElementById("main-show");
-        if (name !== "\xd6vrigt" && name !== "Sammanfattning natt" && name !== "Sammanfattning kv\xe4ll och natt" && name !== "Trafikkontroll" && name !== "Uppdatering") {
-            let article = document.createElement("article");
-            article.classList.add("handelser");
-            mainEL.appendChild(article);
-            article.innerHTML += `
-                <h2>${name}</h2>
-                <h3>${summary}</h3>
-                <h5>${textNew[0]}  ${textNew[2]}</h5>
-                <div id="map-id-${mapCount}" class="map"> <!-- Anv\xe4nder mapCount f\xf6r att skapa unika ID:n -->
-                    <div>
-                `;
-            let map = L.map(`map-id-${mapCount}`).setView([
-                lat,
-                lon
-            ], 13);
-            mapCount++;
-            L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-                maxZoom: 19,
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(map);
-            L.marker([
-                lat,
-                lon
-            ]).addTo(map).bindPopup(`H\xe4r har h\xe4ndelsen skett`).openPopup();
-        }
+        let article = document.createElement("article");
+        article.classList.add("handelser");
+        mainEL.appendChild(article);
+        article.innerHTML += `
+            <h2>${name}</h2>
+            <h3>${summary}</h3>
+            <h5>${textNew[0]}  ${textNew[2]}</h5>
+            <div id="map-id-${mapCount}" class="map"> <!-- Anv\xe4nder mapCount f\xf6r att skapa unika ID:n -->
+                <div>
+            `;
+        let map = L.map(`map-id-${mapCount}`).setView([
+            lat,
+            lon
+        ], 13);
+        mapCount++;
+        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        L.marker([
+            lat,
+            lon
+        ]).addTo(map).bindPopup(`H\xe4r har h\xe4ndelsen skett`).openPopup();
     });
+    displayedEvents += eventsPerPage;
 }
 getData();
+function displayMoreEvents() {
+    getData();
+}
 
 },{}]},["2wcJB","dcsaV"], "dcsaV", "parcelRequirecef7")
 
--- src/js/js.js
+++ src/js/js.js
@@ -1,17 +1,23 @@
-let mapCount = 0; 
+let mapCount = 0;
+let displayedEvents = 0;
+let eventsPerPage = 10;
+let visaMerEl = document.getElementById("visa-mer-data")
+visaMerEl.addEventListener("click", displayMoreEvents);
+
 
 async function getData() {
     try {
         let response = await fetch("https://polisen.se/api/events");
         let data = await response.json();
-        getDisplay(data);
+        displayData(data);
     } catch (error) {
         console.log('Error', error);
     }
 }
 
-function getDisplay(data) {
-    data.forEach(item => {
+function displayData(data) {
+    const mainEL = document.getElementById("main-show");
+    data.slice(displayedEvents, displayedEvents + eventsPerPage).forEach(item => {
         let name = item.type;
         let summary = item.summary;
         let text = item.name;
@@ -21,29 +27,32 @@ function getDisplay(data) {
         let lat = lonlat[0];
         let lon = lonlat[1];
 
-        let mainEL = document.getElementById("main-show");
-        if (name !== "Övrigt" && name !== "Sammanfattning natt" && name !== "Sammanfattning kväll och natt" && name !== "Trafikkontroll" && name !== "Uppdatering") {
-            let article = document.createElement("article");
-            article.classList.add("handelser");
-            mainEL.appendChild(article);
-            article.innerHTML += `
-                <h2>${name}</h2>
-                <h3>${summary}</h3>
-                <h5>${textNew[0]}  ${textNew[2]}</h5>
-                <div id="map-id-${mapCount}" class="map"> <!-- Använder mapCount för att skapa unika ID:n -->
-                    <div>
-                `;
-            let map = L.map(`map-id-${mapCount}`).setView([lat, lon], 13);
-            mapCount++; 
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 19,
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(map);
-            L.marker([lat, lon]).addTo(map)
-                .bindPopup(`Här har händelsen skett`)
-                .openPopup();
-        }
+        let article = document.createElement("article");
+        article.classList.add("handelser");
+        mainEL.appendChild(article);
+        article.innerHTML += `
+            <h2>${name}</h2>
+            <h3>${summary}</h3>
+            <h5>${textNew[0]}  ${textNew[2]}</h5>
+            <div id="map-id-${mapCount}" class="map"> <!-- Använder mapCount för att skapa unika ID:n -->
+                <div>
+            `;
+        let map = L.map(`map-id-${mapCount}`).setView([lat, lon], 13);
+        mapCount++; 
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        L.marker([lat, lon]).addTo(map)
+            .bindPopup(`Här har händelsen skett`)
+            .openPopup();
     });
+    displayedEvents += eventsPerPage;
 }
 
 getData();
+
+
+function displayMoreEvents() {
+    getData();
+}
